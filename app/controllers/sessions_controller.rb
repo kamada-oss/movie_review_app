6e0f2beab94b731f40242be3d020007ec34bcaec
kamada <kamada.engineer@gmail.com>
2020-02-07 11:29:07 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = "メールアドレスもしくはパスワードが不正です"
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    logout
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200203104945) do
+ActiveRecord::Schema.define(version: 20200206111331) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200203104945) do
     t.string "password_digest"
     t.string "nickname"
     t.boolean "agreement"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
