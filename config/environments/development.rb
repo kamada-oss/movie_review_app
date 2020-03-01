df7e18d1796863e7f15c0ab1fc45f09ef5ebdf5c
kamada <kamada.engineer@gmail.com>
2020-03-01 05:55:12 +0000

    Add password reset4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'https:f1b757c6b5ae47edb64af4d127b200b0.vfs.cloud9.us-east-2.amazonaws.com/'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'aaa', protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
